b029111386c4f55901148d570d4d3afbbda4303e
Steven Dubé <77smu19@protonmail.com>
2024-02-18 09:16:01 -0500

    Add cors

--- server/server.js
+++ server/server.js
@@ -1,12 +1,31 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const cookieParser = require('cookie-parser');
+const cors = require('cors')
 dotenv.config({ path: '../.env' });
 const app = express();
 
 const port = process.env.PORT;
+const corsOptions = {
+    origin: '*',
+    optionsSuccessStatus: 200
+}
+app.use(cors(corsOptions));
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
-    res.send("Server is running");
+    try {
+        res.cookie('test', 'test',{
+            httpOnly: true,
+            secure: true
+        })
+        res.status(200)
+            .json({
+                "Test" : "Test json"
+            });
+    } catch(err) {
+        res.status(500)
+    }
 })
 
 app.listen(port, () => {
